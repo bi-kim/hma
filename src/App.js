046cce3acce1a4e3ca90f4ce8f991339c0610dea
Bi kim <kimbi@outlook.kr>
2018-03-25 16:59:28 +0900

    추가; Route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
-
+import { Route, Switch } from 'react-router-dom';
+import { Container } from 'semantic-ui-react'
+import Navigation from './components/Nav.component';
+import DialogTaskCreate from './components/DialogTaskCreate.component';
+import HealthActivity from './activity/health.activity';
+import MonitorActivity from './activity/monitor.activity';
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navigation />
+        <DialogTaskCreate />
+        <Container>
+          <Switch path="/">
+            <Route exact path='/' render={() => <HealthActivity />}/>
+            <Route exact path='/health' render={() => <HealthActivity />}/>
+            <Route exact path='/monitor' render={() => <MonitorActivity />}/>
+          </Switch>
+        </Container>
       </div>
     );
   }
